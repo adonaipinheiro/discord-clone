66475309b5cb7c53d54e6aec3ad5a188c9daa15e
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-06-26 16:21:00 -0300

    Initial Commit

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton';
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={3} />
+      <ServerButton  />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={72} />
+    </Container>
+  );
+}
+
+export default ServerList;
